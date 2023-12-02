9053d04ac0e84da57c50e5d825534b4ebfa13a4b
VPolikushin94 <dj0j@yandex.ru>
2023-12-02 17:20:35 +0300

    Убрал lateinit var

--- app/src/main/java/ru/practicum/android/diploma/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/root/RootActivity.kt
@@ -11,11 +11,12 @@ import ru.practicum.android.diploma.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityRootBinding
+    private var _binding: ActivityRootBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityRootBinding.inflate(layoutInflater)
+        _binding = ActivityRootBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         // Пример использования access token для HeadHunter API
