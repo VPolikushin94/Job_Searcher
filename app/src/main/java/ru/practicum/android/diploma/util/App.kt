e106adfbdd4900178a0254e38d1ed6b070295ed8
dfater <dfater4@yandex.ru>
2023-12-02 14:21:18 +0300

    Создать свой класс Application: Удалил все.

--- app/src/main/java/ru/practicum/android/diploma/util/App.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/App.kt
@@ -1,17 +1,10 @@
 package ru.practicum.android.diploma.util
 
 import android.app.Application
-import org.koin.android.ext.koin.androidContext
-import org.koin.core.context.GlobalContext
 
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-
-        GlobalContext.startKoin {
-            androidContext(this@App)
-            modules()
-        }
     }
 }
