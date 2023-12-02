5e20f6849df637f19afef828305d8c36fd2263c8
dfater <dfater4@yandex.ru>
2023-12-02 14:58:33 +0300

    Создать свой класс Application: Удалил строку.

--- app/src/main/java/ru/practicum/android/diploma/core/App.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/App.kt
@@ -4,7 +4,6 @@ import android.app.Application
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
-
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
