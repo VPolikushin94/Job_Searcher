64c6eddc735c41e65c8ec3e833a1fa81655c23e3
VPolikushin94 <dj0j@yandex.ru>
2023-12-02 17:15:39 +0300

    Bottom_nav_view. Добавил BottomNavVew и Separator над ним. Написал в RootActivity.kt функцию setBottomNavigation() обработки нажатия на кнопки BottomNavVew

--- app/src/main/java/ru/practicum/android/diploma/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/root/RootActivity.kt
@@ -2,16 +2,48 @@ package ru.practicum.android.diploma.root
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRootBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_root)
+        binding = ActivityRootBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // Пример использования access token для HeadHunter API
         networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
+
+        setBottomNavigation()
+    }
+
+    private fun setBottomNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.main_fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNav.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.filterSettingsFragment -> {
+                    binding.bottomNav.isVisible = false
+                    binding.separator.isVisible = false
+                }
+
+                else -> {
+                    binding.bottomNav.isVisible = true
+                    binding.separator.isVisible = true
+                }
+            }
+        }
     }
 
     private fun networkRequestExample(accessToken: String) {
