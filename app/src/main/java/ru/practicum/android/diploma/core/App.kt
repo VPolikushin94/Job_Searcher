40d3d0f4d542999eb82b77ff725a1b793736e139
dfater <dfater4@yandex.ru>
2023-12-02 14:54:55 +0300

    Создать свой класс Application: Добавил startKoin, перенес в core.

---
+++ app/src/main/java/ru/practicum/android/diploma/core/App.kt
@@ -0,0 +1,17 @@
+package ru.practicum.android.diploma.core
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@App)
+            modules()
+        }
+    }
+}
