1b6641c6f15fcebbd6a6cb089faf84ad37d4bfdf
dfater <dfater4@yandex.ru>
2023-12-03 22:36:09 +0300

    Clean: исправления.

--- app/src/main/java/ru/practicum/android/diploma/core/App.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/App.kt
@@ -3,6 +3,10 @@ package ru.practicum.android.diploma.core
 import android.app.Application
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+import ru.practicum.android.diploma.di.dataModule
+import ru.practicum.android.diploma.di.interactorModule
+import ru.practicum.android.diploma.di.repositoryModule
+import ru.practicum.android.diploma.di.viewModeModule
 
 class App : Application() {
     override fun onCreate() {
@@ -10,7 +14,7 @@ class App : Application() {
 
         startKoin {
             androidContext(this@App)
-            modules()
+            modules(dataModule, interactorModule, repositoryModule, viewModeModule)
         }
     }
 }
