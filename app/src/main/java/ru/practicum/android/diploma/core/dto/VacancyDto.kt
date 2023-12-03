81cfa02e034e126f63c2ce23af9eae4900f66b76
alferac@inbox.ru <dmitr-nik.k@yandex.ru>
2023-12-03 21:10:50 +0500

    Diplom-22: components date layer, change 1

--- app/src/main/java/ru/practicum/android/diploma/core/dto/VacancyDto.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/dto/VacancyDto.kt
@@ -1,11 +1,28 @@
 package ru.practicum.android.diploma.core.dto
 
 data class VacancyDto(
-    val vacancyId: String,
-    val name: String,
-    val employer: String,
-    val area: String,
-    val salaryFrom: String,
-    val salaryTo: String,
-    val currency: String
+    val id: String,
+    val name: String?,
+    val employer: EmployerDto,
+    val areaDto: AreasDto,
+    val salary: SalaryDto
+)
+
+data class SalaryDto(
+    val currency: String?,
+    val from: Int?,
+    val gross: Boolean,
+    val to: Int?
+)
+
+data class AreasDto(
+    val id: String,
+    val name: String?
+)
+
+data class EmployerDto(
+    val id: String,
+    val name: String?,
+    val url: String?,
+    val logo_urls: Map<String, String?>
 )
