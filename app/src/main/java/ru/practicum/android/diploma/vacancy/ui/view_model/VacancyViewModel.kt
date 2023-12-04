5cbfd715519eab46c2e8547b5c470edc123e6f78
VPolikushin <vipolikushin@gmail.com>
2023-12-04 16:34:05 +0300

    добавил todo

--- app/src/main/java/ru/practicum/android/diploma/vacancy/ui/view_model/VacancyViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/vacancy/ui/view_model/VacancyViewModel.kt
@@ -3,5 +3,5 @@ package ru.practicum.android.diploma.vacancy.ui.view_model
 import androidx.lifecycle.ViewModel
 
 class VacancyViewModel : ViewModel() {
-    //TODO("Not yet implemented")
+    // TODO: Not yet implemented
 }
