fde1930d53b7765f51ae623995b8faea98063ac2
alferac@inbox.ru <dmitr-nik.k@yandex.ru>
2023-12-10 14:22:32 +0500

    Diploma-54: add domain class

--- app/src/main/java/ru/practicum/android/diploma/vacancy/domain/model/DetailsVacancy.kt
+++ app/src/main/java/ru/practicum/android/diploma/vacancy/domain/model/DetailsVacancy.kt
@@ -1,5 +1,24 @@
 package ru.practicum.android.diploma.vacancy.domain.model
 
 data class DetailsVacancy(
-    val id: String
+    val id: String,
+    val name: String,
+    val employerId: String?,
+    val employerName: String?,
+    val employerLogo: String?,
+    val employerCity: String?,
+    val employmentId: String?,
+    val employmentName: String?,
+    val experienceId: String?,
+    val experienceName: String?,
+    val salaryTo: String?,
+    val salaryFrom: String?,
+    val salaryCurrency: String?,
+    val address: String?,
+    val description: String?,
+    val keySkills: String?,
+    val contactPerson: String?,
+    val eMail: String?,
+    val telephone: String?,
+    val comment: String?
 )
