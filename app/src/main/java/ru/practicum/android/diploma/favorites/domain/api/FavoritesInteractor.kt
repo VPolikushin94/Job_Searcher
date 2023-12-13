f8ea7a56117600ee2e46fe493bd78d952a091e7d
VPolikushin <vipolikushin@gmail.com>
2023-12-13 15:19:20 +0300

    убрал пробелы

--- app/src/main/java/ru/practicum/android/diploma/favorites/domain/api/FavoritesInteractor.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorites/domain/api/FavoritesInteractor.kt
@@ -1,8 +1,8 @@
 package ru.practicum.android.diploma.favorites.domain.api
 
 import kotlinx.coroutines.flow.Flow
-import ru.practicum.android.diploma.favorites.domain.models.Resource
 import ru.practicum.android.diploma.core.models.SearchedVacancy
+import ru.practicum.android.diploma.favorites.domain.models.Resource
 
 interface FavoritesInteractor {
 
--- app/src/main/java/ru/practicum/android/diploma/favorites/domain/impl/FavoritesInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorites/domain/impl/FavoritesInteractorImpl.kt
@@ -2,9 +2,9 @@ package ru.practicum.android.diploma.favorites.domain.impl
 
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import ru.practicum.android.diploma.core.models.SearchedVacancy
 import ru.practicum.android.diploma.favorites.domain.api.FavoritesInteractor
 import ru.practicum.android.diploma.favorites.domain.models.Resource
-import ru.practicum.android.diploma.core.models.SearchedVacancy
 
 class FavoritesInteractorImpl : FavoritesInteractor {
     override fun getVacancyList(): Flow<Resource<List<SearchedVacancy>>> {
