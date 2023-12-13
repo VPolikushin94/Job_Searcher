6bf855c05b94b296408bf21c8024008e5732ab14
dfater <dfater4@yandex.ru>
2023-12-13 21:20:09 +0300

    Рефактор детекта

--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
@@ -7,7 +7,6 @@ import ru.practicum.android.diploma.filter.ui.viewmodel.FiltrationLocationCountr
 import ru.practicum.android.diploma.filter.ui.viewmodel.FiltrationLocationViewModel
 import ru.practicum.android.diploma.filter.ui.viewmodel.FiltrationViewModel
 import ru.practicum.android.diploma.favorites.ui.viewmodel.FavoritesViewModel
-import ru.practicum.android.diploma.filter.ui.viewmodel.FilterViewModel
 import ru.practicum.android.diploma.search.ui.viewmodel.SearchViewModel
 import ru.practicum.android.diploma.vacancy.ui.viewmodel.VacancyViewModel
 
@@ -38,8 +37,10 @@ val viewModeModule = module {
     viewModel {
         FiltrationIndustryViewModel()
     }
-    
+
+    viewModel {
         FavoritesViewModel(
             favoritesInteractor = get()
         )
+    }
 }
