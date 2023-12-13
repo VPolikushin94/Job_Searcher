147bfb25567167efc2ce362805d00ae51fa451aa
dfater <dfater4@yandex.ru>
2023-12-13 22:29:13 +0300

    Рефактор детекта 4

--- app/src/main/java/ru/practicum/android/diploma/filter/ui/fragment/FiltrationIndustryFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/ui/fragment/FiltrationIndustryFragment.kt
@@ -4,12 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.databinding.FragmentFiltrationIndustryBinding
-import ru.practicum.android.diploma.filter.ui.models.FilterIndustryScreenState
 import ru.practicum.android.diploma.filter.ui.viewmodel.FiltrationIndustryViewModel
 
 class FiltrationIndustryFragment : Fragment() {
