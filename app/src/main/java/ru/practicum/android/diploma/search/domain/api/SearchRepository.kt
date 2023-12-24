48314692469a00b18e1f0d5637bb8d7970290810
dfater <dfater4@yandex.ru>
2023-12-24 12:13:27 +0300

    Рефактор

--- app/src/main/java/ru/practicum/android/diploma/search/domain/api/SearchRepository.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/domain/api/SearchRepository.kt
@@ -1,7 +1,6 @@
 package ru.practicum.android.diploma.search.domain.api
 
 import kotlinx.coroutines.flow.Flow
-import ru.practicum.android.diploma.core.models.Vacancy
 import ru.practicum.android.diploma.search.domain.models.Resource
 import ru.practicum.android.diploma.search.domain.models.SearchVacancyResult
 import ru.practicum.android.diploma.search.domain.models.VacancySearchParams
